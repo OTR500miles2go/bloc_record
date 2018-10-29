a72daf86f5727c9a71aab847af404e82493584d3
OTR500miles2go <32044213+OTR500miles2go@users.noreply.github.com>
2018-10-28 23:42:23 -0500

    Associations assignment complete

--- lib/bloc_record/associations.rb
+++ lib/bloc_record/associations.rb
@@ -17,7 +17,18 @@ module Associations
       end
 
       collection
-    end    
+    end
+  end
+
+  def has_one(association)
+    define_method(association) do
+      row = self.class.connection.get_first_row <<-SQL
+        SELECT * FROM #{association.to_s.singularize}
+        WHERE #{self.class.table}_id = #{self.id}
+      SQL
+      class_name = association.to_s.classify.constantize
+      class_name.new(Hash[class_name.columns.zip(row)])
+    end
   end
 
   def belongs_to(association)
@@ -35,5 +46,5 @@ module Associations
         class_name.new(data)
       end
     end
-  end  
+  end
 end
